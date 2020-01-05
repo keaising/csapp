c9225b07c3824d462f7973f31f0afcb15d6ef0c0
keaising <keaising@gmail.com>
2020-01-05 12:17:59 +0800

    fmt print result

--- chapter2/e2.3.py
+++ chapter2/e2.3.py
@@ -16,7 +16,7 @@ if number.startswith('0b') and base == 10:
 
 x = int(number, base)
 
-print('base 2: ', bin(x))
-print('base 8: ', oct(x))
+print('base 2:  ', bin(x))
+print('base 8:  ', oct(x))
 print('base 10: ', x)
 print('base 16: ', hex(x))
