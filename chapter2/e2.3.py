4fd152a8c6df955a8f6d51192e0dbb9040d927b2
keaising <keaising@gmail.com>
2020-01-05 12:05:29 +0800

    init

---
+++ chapter2/e2.3.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+import sys
+
+number = sys.argv[1]
+base = 10
+
+if sys.argv.__len__() == 3:
+	base = int(sys.argv[2])
+
+if number.startswith('0x'):
+	base = 16
+
+if number.startswith('0b') and base == 10:
+	base = 2
+
+x = int(number, base)
+
+print('base 2: ', bin(x))
+print('base 8: ', oct(x))
+print('base 10: ', x)
+print('base 16: ', hex(x))
